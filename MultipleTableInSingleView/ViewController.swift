3e3cecb28e89451b1b4ff2e85ab2ce4d6a253489
SAYDUR <saydur@labs-mac.local>
2020-01-06 17:08:25 +0600

    Dictionary Data Added

--- MultipleTableInSingleView/ViewController.swift
+++ MultipleTableInSingleView/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        var dictionaryData = ["A":["Apple","Ant"],
+                              "B":["Banana","bat"],
+                              "C":["cat","cow"],
+                              "D":["dog","dew"],
+                              "E":["eye","elephant"]]
+        
+        print(dictionaryData)
+        
+        super.viewDidLoad()
     }
-
-
+    
+    
 }
 
